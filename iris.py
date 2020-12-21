62ce6662e21cf7e27f8d8e480a6ee9ba412bd2f6
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-12-21 00:04:29 -0500

    set up training and test data sets.

--- iris.py
+++ iris.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_iris
-from sklearn.model import train_test_split
+from sklearn.model_selection import train_test_split
 
 iris = load_iris()
 # print(iris)
@@ -17,3 +17,10 @@ feature_names = iris.feature_names
 target_names = iris.target_names
 print(feature_names)
 print(target_names)
+
+# Traning data for the computer to learn.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+print(X_train.shape)
+print(X_test.shape)
+# Both of these print statement gives us, (rows, 4(dimensionality)).
+# (90, 4)  means 90 rows from the test_size over its dimensionality.
